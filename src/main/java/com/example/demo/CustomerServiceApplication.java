3a7a04e2bc11d1e19faa1e83f8ae462f7f5d8318
Mounika Manchala <mounikamanchala25@gmail.com>
2023-06-13 10:19:10 +0530

    removed security & added method for getting the port

--- src/main/java/com/example/demo/CustomerServiceApplication.java
+++ src/main/java/com/example/demo/CustomerServiceApplication.java
@@ -1,15 +1,30 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class CustomerServiceApplication {
+
+	@Autowired
+	private Environment environment;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@GetMapping("/status")
+	public ResponseEntity<?> getPort() {
+		return ResponseEntity
+				.ok("customer service up & running with port " + environment.getProperty("local.server.port"));
 	}
 
 }
